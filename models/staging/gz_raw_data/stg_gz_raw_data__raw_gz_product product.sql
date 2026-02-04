31cde0959e1ce19137cb8379840083dc65b35553
zoe.lebras.bagoe@gmail.com <zoe.lebras.bagoe@gmail.com>
2026-02-04 13:37:53 +0000

    update

--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_product product.sql
+++
@@ -1,5 +0,0 @@
-select
-    CAST (purchSE_PRICE as float64) as purchase_price,
-    SUM (products_id)
-from {{ source('gz_raw_data', 'raw_gz_product product') }}
-    GROUP BY purchase_price
--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_sales sales.sql
+++
@@ -1,7 +0,0 @@
-select
-    date_date,
-    orders_id,
-    pdt_id as prodct_id,
-    revenue,
-    quantity,
-from {{ source('gz_raw_data', 'raw_gz_sales sales') }}
