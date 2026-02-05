deb26d21926ad32a7dcdeb44716ec9560048aa07
Zoé Le Bras <zoe.lebras.bagoe@gmail.com>
2026-02-05 10:14:25 +0000

    update

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,11 @@
 SELECT 
     orders_id,
     date_date,
+    products_id,
     ROUND(SUM(revenue),2) AS revenue,
     ROUND(SUM(quantity),2) AS quantity,
     ROUND(SUM(purchase_cost),2) AS purchase_cost,
     ROUND(SUM(margin),2) AS margin
 FROM {{ ref('int_sales_margin') }}
- GROUP BY orders_id, date_date
+ GROUP BY orders_id, date_date, products_id
  ORDER BY orders_id DESC
